d052e09199af9962bbe26af6430d919695fb57b6
Evita Coelho <evita29687@gmail.com>
2024-04-05 19:38:18 +0100

    Add project files.

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace NZWalks.API.Controllers
+{
+    //https://localhost:portnumber/api/students/
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        //GET :  //https://localhost:portnumber/api/students/
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            //returns all students 
+            string[] studentNames = new string[] { "Luna", "Kaos", "Ryan", "Rachel", "Monica" };
+            //return status code and student names to the caller
+            return Ok(studentNames);
+        }
+    }
+}
---
+++ NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext 
+    {
+        // this is used by the controllers to talk to the DB
+        //pass the db context options to the base class
+        public NZWalksDbContext(DbContextOptions dbContextOptions) : base (dbContextOptions)
+        {
+            
+        }
+        //DbSet represents a collection of entities in the DB i.e. domain models (Walk, Region, Difficulty)
+        //Naming Convention: Prop names are plural of the domain name - creates tables within the DB based on the domain model
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+
+    }
+}
